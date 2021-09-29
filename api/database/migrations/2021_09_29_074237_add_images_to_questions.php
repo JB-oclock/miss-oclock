ef69e94d6b49a4eaefeaf27a44dc7a70bf391a34
Fwagra <fabiocerqueiracw@gmail.com>
2021-09-29 11:11:50 +0200

    Fix migration error

--- api/database/migrations/2021_09_29_074237_add_images_to_questions.php
+++ api/database/migrations/2021_09_29_074237_add_images_to_questions.php
@@ -26,7 +26,7 @@ class AddImagesToQuestions extends Migration
     public function down()
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->dropColumn('correct_answer');
+            $table->dropColumn('image');
         });
     }
 }
