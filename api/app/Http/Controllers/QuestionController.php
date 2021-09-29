0c45fc8fb2e5adf5ddba1bfaf26411f6f8854705
Fwagra <fabiocerqueiracw@gmail.com>
2021-09-29 11:11:02 +0200

    Upload image for questions

--- api/app/Http/Controllers/QuestionController.php
+++ api/app/Http/Controllers/QuestionController.php
@@ -21,12 +21,28 @@ class QuestionController extends Controller
 
     public function createpost(Request $request)
     {
+        $request->validate([
+            'title' => 'required',
+            'answer_1' => 'required',
+            'answer_2' => 'required',
+            'answer_3' => 'required',
+            'answer_good' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+
         $question = new Question();
         $question->title = $request->input('title');
         $question->answer_1 = $request->input('answer_1');
         $question->answer_2 = $request->input('answer_2');
         $question->answer_3 = $request->input('answer_3');
         $question->answer_good = $request->input('answer_good');
+
+        if($request->has('image')){
+            $path = $request->image->store('question_images', 'public');
+            $question->image = $path;
+        }
+
         $question->save();
         return redirect()->back();
     }
@@ -38,11 +54,27 @@ class QuestionController extends Controller
 
     public function editpost(Question $question, Request $request)
     {
+        $request->validate([
+            'title' => 'required',
+            'answer_1' => 'required',
+            'answer_2' => 'required',
+            'answer_3' => 'required',
+            'answer_good' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+
         $question->title = $request->input('title');
         $question->answer_1 = $request->input('answer_1');
         $question->answer_2 = $request->input('answer_2');
         $question->answer_3 = $request->input('answer_3');
         $question->answer_good = $request->input('answer_good');
+
+        if($request->has('image')){
+            $path = $request->image->store('question_images', 'public');
+            $question->image = $path;
+        }
+
         $question->save();
         return redirect()->back();
     }
--- api/resources/views/question/create.blade.php
+++ api/resources/views/question/create.blade.php
@@ -13,10 +13,13 @@
                   @endisset
                 </div>
                 <div class="card-body">
+                  @foreach($errors->all() as $message)
+                      <div class="alert alert-danger">{{ $message }}</div>
+                  @endforeach
                   @isset ($question)
-                    {{ Form::model($question, array('route' => array('edit-question-post', $question->id))) }}
+                    {{ Form::model($question, array('route' => array('edit-question-post', $question->id), 'files' => true)) }}
                   @else
-                    {{ Form::open(['route' => 'create-question-post']) }}
+                    {{ Form::open(['route' => 'create-question-post', 'files' => true]) }}
                   @endisset
 
                   <div class="form-group">
@@ -36,6 +39,13 @@
                     {{ Form::text('answer_2', null, ['class' => 'form-control']) }}
                     {{ Form::label('answer_3', 'Autre proposition - 3') }}
                     {{ Form::text('answer_3', null, ['class' => 'form-control']) }}
+                  </div>
+                  <div class="form-group">
+                    {!! Form::label('image', "Image à afficher sur l'écran de présentation de réponse") !!}
+                    {!! Form::file('image', ['class' => 'form-control']) !!}
+                    @if(isset($question) && !empty($question->image))
+                        <img width="200" src="{{ asset('/storage/'.$question->image) }}" >
+                    @endif
                   </div>
 
                   <div class="form-group">
